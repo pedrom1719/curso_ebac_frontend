6643f95d51a9d6391dcbf14dd2867475f9bb44ef
Pedro <39057776+pedrom1719@users.noreply.github.com>
2024-09-11 19:41:36 -0300

    add: Atividade EBAC - Agenda de Contatos

---
+++ atv-agenda/js.js
@@ -0,0 +1,24 @@
+const formButton = document.querySelector('button[type="submit"]');
+
+let fullTable = "";
+
+formButton.addEventListener('click', function(e){
+    e.preventDefault();
+
+    const inputNome = document.querySelector('#nome-contato');
+    const inputTel = document.querySelector('#numero-contato');
+
+    if(!inputNome.value == "" && !inputTel.value == ""){
+        let rowsTable = '';
+        rowsTable = `<tbody><tr><td>${inputNome.value}</td></tr><tr><td>${inputTel.value}</td></tr></tbody>`;
+    
+        fullTable += rowsTable;
+    
+        document.querySelector('#contentTable').innerHTML = fullTable;
+    
+        // inputNome.value = "";
+        // inputTel.value = "";
+    }else{
+        alert('Preencha todos os campos corretamente.');
+    }
+});
