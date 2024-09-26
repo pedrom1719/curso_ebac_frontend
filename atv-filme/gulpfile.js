989fe8a40010b980e04526e769c5e7db799ae1eb
Pedro <39057776+pedrom1719@users.noreply.github.com>
2024-09-26 17:33:31 -0300

    add: remaining files

---
+++ atv-filme/gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const pluginSass = require('gulp-sass')(require('sass'));
+const pluginUglify = require('gulp-uglify');
+const pluginImageMin = require('gulp-imagemin');
+
+function compSass(){
+    return gulp.src('./src/style/**/*.scss')
+        .pipe(pluginSass({ outputStyle: 'compressed'}))
+        .pipe(gulp.dest('./dist/style'));
+}
+
+function compJS(){
+    return gulp.src('./src/js/**/*.js')
+        .pipe(pluginUglify())
+        .pipe(gulp.dest('dist/js'));
+}
+
+function compImages(){
+    return gulp.src('./src/img/**/*')
+    .pipe(pluginImageMin())
+    .pipe(gulp.dest('dist/img'));
+}
+
+exports.default = function(){
+    gulp.watch('./src/style/**/*.scss', {ignoreInitial: false}, gulp.series(compSass));
+    gulp.watch('./src/js/**/*.js', {ignoreInitial: false}, gulp.series(compJS));
+    gulp.watch('./src/img/**/*', {ignoreInitial: false}, gulp.series(compImages));
+}
