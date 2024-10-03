15595c28957f19d8930141ed3d0cd39419319eba
Pedro <39057776+pedrom1719@users.noreply.github.com>
2024-10-03 17:35:58 -0300

    add: exercicio typescript

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function numMultiply(a, b) {
+    return a * b;
+}
+function nameGreeting(nome) {
+    return 'Olá, ' + nome;
+}
+console.log(numMultiply(5, 5));
+console.log(nameGreeting('João'));
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+function numMultiply(a:number, b:number):number {
+    return a * b;
+}
+
+function nameGreeting(nome:string) :string {
+    return 'Olá, ' + nome;
+}
+
+console.log(numMultiply(5, 5));
+console.log(nameGreeting('João'))
